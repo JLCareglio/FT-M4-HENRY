b959128d85f5b9df732626a3a4d9f950a7321827
Jesús Lautaro Careglio Albornoz <j.l.careglio.albornoz@gmail.com>
2022-11-09 00:21:49 -0300

    🧪 tests descomentados

--- 03-sequelize/homework/tests/character-routes.spec.js
+++ 03-sequelize/homework/tests/character-routes.spec.js
@@ -49,13 +49,13 @@ describe('Character Routes', () => {
       const p2Character = Character.create({code: 'TWO', name: 'Second', hp: 135.0, mana: 40.0, race: 'Machine', age: 20});
       const p3Character = Character.create({code: 'THREE', name: 'Third', hp: 110.0, mana: 110.0, race: 'Human', age: 23});
       const [p1, p2, p3] = await Promise.all([p1Character, p2Character, p3Character]);
-      // await Promise.all([
-      //   p1.createRole({name: 'Tank'}),
-      //   p1.createRole({name: 'Top'}),
-      //   p2.createRole({name: 'Jungle'}),
-      //   p3.createRole({name: 'Mid'}),
-      //   p3.createRole({name: 'Support'})
-      // ]);
+      await Promise.all([
+        p1.createRole({name: 'Tank'}),
+        p1.createRole({name: 'Top'}),
+        p2.createRole({name: 'Jungle'}),
+        p3.createRole({name: 'Mid'}),
+        p3.createRole({name: 'Support'})
+      ]);
     })
 
     describe('Parte UNO', () => {
